706e29a5217424064ee2de2a6f4d0feb4927da11
Muhammad Sheryar <132276541+Sheryar-bit@users.noreply.github.com>
2024-08-13 15:05:36 +0500

    Add files via upload

---
+++ Comparisions.js
@@ -0,0 +1,17 @@
+console.log(null > 0)
+// false
+console.log(null == 0)
+// false
+console.log(null >= 0)
+// true
+
+console.log(undefined == 0)
+// false
+console.log(undefined > 0)
+// false
+console.log(undefined < 0)
+// false
+
+// === (checks the Datatype also) it is called strict chaecking
+console.log("2" === 2)
+// false
