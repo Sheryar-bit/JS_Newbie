f1a791b9b5df29bc2920eaa4da2fb3420661d705
Sheryar <alisharyar93@gmail.com>
2024-10-23 22:02:22 +0500

    response and requests

--- Working_With_Node_JS/app.js
+++ Working_With_Node_JS/app.js
@@ -1,3 +1,39 @@
+const GithubDATA = {
+    "login": "Sheryar-bit",
+    "id": 132276541,
+    "node_id": "U_kgDOB-JhPQ",
+    "avatar_url": "https://avatars.githubusercontent.com/u/132276541?v=4",
+    "gravatar_id": "",
+    "url": "https://api.github.com/users/Sheryar-bit",
+    "html_url": "https://github.com/Sheryar-bit",
+    "followers_url": "https://api.github.com/users/Sheryar-bit/followers",
+    "following_url": "https://api.github.com/users/Sheryar-bit/following{/other_user}",
+    "gists_url": "https://api.github.com/users/Sheryar-bit/gists{/gist_id}",
+    "starred_url": "https://api.github.com/users/Sheryar-bit/starred{/owner}{/repo}",
+    "subscriptions_url": "https://api.github.com/users/Sheryar-bit/subscriptions",
+    "organizations_url": "https://api.github.com/users/Sheryar-bit/orgs",
+    "repos_url": "https://api.github.com/users/Sheryar-bit/repos",
+    "events_url": "https://api.github.com/users/Sheryar-bit/events{/privacy}",
+    "received_events_url": "https://api.github.com/users/Sheryar-bit/received_events",
+    "type": "User",
+    "user_view_type": "public",
+    "site_admin": false,
+    "name": "Muhammad Sheryar",
+    "company": "DHA SUFFA UNIVERSITY",
+    "blog": "",
+    "location": "Pakistan",
+    "email": null,
+    "hireable": null,
+    "bio": "Shaping Future Algorithms | Campus Ambassador @ GSSoC '24 | Open Source Contributor",
+    "twitter_username": null,
+    "public_repos": 19,
+    "public_gists": 0,
+    "followers": 3,
+    "following": 4,
+    "created_at": "2023-05-01T13:44:56Z",
+    "updated_at": "2024-09-28T09:24:08Z"
+}
+
 require('dotenv').config()
 const express = require('express')
 const app = express()
@@ -17,6 +53,10 @@ app.get("/login", function(req, res) {
 
 app.get('/youtube', function(req, res) {
     res.send('<h1>welcome to YouTube</h2>')
+})
+
+app.get('/github', function(req, res) {
+    res.json(GithubDATA)
 })
 
 app.listen(process.env.port, function() {
