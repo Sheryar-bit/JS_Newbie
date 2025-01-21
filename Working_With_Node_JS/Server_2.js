51bd5ce02a4c1e06cfdebeb18c6897683ecc553b
Sheryar <alisharyar93@gmail.com>
2025-01-21 13:02:26 +0500

    Hotel_Server

--- Working_With_Node_JS/Server_2.js
+++ Working_With_Node_JS/Server_2.js
@@ -4,7 +4,23 @@ const db2 = require('./db2')
 const cors = require ('cors')
 require('dotenv').config();
 
+//Import Router files
+const PersonRoutes = require('./Routes/PersonRoutes');
+const MenuItemRoutes = require('./Routes/MenuItemRoutes');
+
+
 app.use(cors({ origin: 'http://127.0.0.1:5500' })); // Allow requests from your frontend, "http://127.0.0.1:5500" = front end port
+
+//Middleware function:
+
+const logReq = function(req, res, next) {
+    console.log(`[${new Date().toLocaleString()}] Request Made to : ${req.originalUrl}`)
+    next(); //Moves on next phase
+}
+app.use(logReq)
+app.get('/', logReq ,function(req, res) {
+    res.send('welcome to BBQ TONIGHT')
+})
 
 
 const bodyParser = require('body-parser')
@@ -13,13 +29,7 @@ app.use(bodyParser.json())   //req.body
 const person = require('./models/person')
 const MenuItem = require('./models/MenuItem')
 
-app.get('/', function(req, res) {
-    res.send('welcome to BBQ TONIGHT')
-})
 
-//Import Router files
-const PersonRoutes = require('./Routes/PersonRoutes');
-const MenuItemRoutes = require('./Routes/MenuItemRoutes');
 
 //use the router
 app.use('/person', PersonRoutes)
